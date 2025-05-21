8ecca2718641fcaab2cc43e38de377a2f4a86c1b
ganshyamsharma <gsmsharma99@gmail.com>
2025-05-21 19:42:45 +0530

    changed naming of variables for easier readability

--- Double_Dabble.v
+++ Double_Dabble.v
@@ -3,7 +3,7 @@
 module binary_to_bcd(
 	input i_clk_1mhz, i_reset,
 	input [12:0] i_binary_data,	
-	output reg [15:0] o_binary_data 
+	output reg [15:0] o_bcd_data 
 );
 	reg [12:0] r_binary_data;
 	reg [3:0] r_bcd1_value, r_bcd2_value, r_bcd3_value, r_bcd4_value, r_conv_counter;
@@ -39,7 +39,7 @@ module binary_to_bcd(
 		else begin 		  
 		  if(r_conv_comp == 1) begin
 		      r_binary_data <= i_binary_data;
-			  o_binary_data <= {r_bcd4_value, r_bcd3_value, r_bcd2_value, r_bcd1_value};
+			  o_bcd_data <= {r_bcd4_value, r_bcd3_value, r_bcd2_value, r_bcd1_value};
 			  r_conv_comp <= 0;
 			  {r_bcd4_value, r_bcd3_value, r_bcd2_value, r_bcd1_value} <= 16'd0;
 		  end
